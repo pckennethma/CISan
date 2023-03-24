6aa5de3bd6a1b0886786646b011f7922b7173fc2
ZhenlanJi <zhenlanji123@gmail.com>
2023-03-24 18:50:40 +0800

    fix bugs

--- OpAblation.py
+++ OpAblation.py
@@ -48,7 +48,7 @@ def EDsan_pc_skl(var_num, independence_func, enable_solver=False):
                     continue
                 if len(neighbors) - 1 < order:
                     continue
-                print(node_x, node_y, edges_to_remove, graph, TOTAL_CI)
+                # print(node_x, node_y, edges_to_remove, graph, TOTAL_CI)
                 cond_set_list = list(combinations(neighbors - {node_y}, order))
                 ci_relation_candidate = [CIStatement.createByXYZ(
                     node_x, node_y, set(cond_set),
@@ -67,9 +67,12 @@ def EDsan_pc_skl(var_num, independence_func, enable_solver=False):
                             break
                 else:
                     for ci in ci_relation_candidate:
-                        x, y, z = list(ci.x)[0], list(ci.y)[0], ci.z
                         TOTAL_CI += 1
-                        if independence_func(x, y, z):
+                        x, y, z = list(ci.x)[0], list(ci.y)[0], ci.z
+                        is_ind = independence_func(x, y, z)
+                        incoming_ci = CIStatement.createByXYZ(x, y, z, is_ind)
+                        kb.AddFact(incoming_ci)
+                        if is_ind:
                             edges_to_remove.add((node_x, node_y))
                             break
         for edge in edges_to_remove:
@@ -84,6 +87,7 @@ def run_detection(
         kb, error_rate: float, seed: int, method_name: str):
     kb.Perturb(error_rate, seed)
     last_ci = kb.facts.pop()
+    error_detected = False
     try:
         kb.EDsan_singleM(last_ci, method_name)
     except AssertionError:
@@ -112,9 +116,12 @@ if __name__ == "__main__":
     dag_path = f"/home/zjiae/Data/benchmarks/{args.benchmark}_graph.txt"
     dag = read_dag(dag_path)
     oracle = OracleCI(dag=dag)
-    _, _, kb = EDsan_pc_skl(dag.get_num_nodes(), oracle.oracle_ci, enable_solver=False)
+    est, total_ci, kb = EDsan_pc_skl(dag.get_num_nodes(), oracle.oracle_ci, enable_solver=False)
+    print("Total CI", total_ci)
+    print("SHD", compare_skeleton(est, dag))
+    print("Start detection")
     for seed in range(total_num):
-        if run_detection(kb, args.error_ratio, seed, args.method_name):
+        if run_detection(kb.copy(), args.error_ratio, seed, args.method_name):
             detected_num += 1
     print(f"Detected {detected_num} out of {total_num} errors")
 
